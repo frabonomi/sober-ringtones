70b7d4c624d134a79a83a54566abc0b3a6ecc704
Francesco Bonomi <frbonomi@gmail.com>
2025-02-05 13:42:35 +0100

    feat: add credits to footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,28 +1,27 @@
 import Image from "next/image";
 import styles from "./footer.module.css";
-import Container from "./container";
 
 import ccLicenseImage from "../../public/by-sa.png";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <Container>
-        <p>
-          All ringtones are licensed under{" "}
-          <a href="https://creativecommons.org/licenses/by-sa/4.0/">
-            CC BY-SA 4.0
-          </a>
-        </p>
+      <p>
+        All ringtones were created by Francesco Bonomi and are distributed under
+        the{" "}
         <a href="https://creativecommons.org/licenses/by-sa/4.0/">
-          <Image
-            src={ccLicenseImage}
-            alt="CC BY-SA 4.0 license icon"
-            style={{ width: "100px", height: "auto", opacity: 0.5 }}
-            sizes="100px"
-          ></Image>
-        </a>
-      </Container>
+          Creative Commons Attribution-ShareAlike 4.0 (CC BY-SA 4.0)
+        </a>{" "}
+        license.
+      </p>
+      <a href="https://creativecommons.org/licenses/by-sa/4.0/">
+        <Image
+          src={ccLicenseImage}
+          alt="CC BY-SA 4.0 license icon"
+          style={{ width: "100px", height: "auto", opacity: 0.5 }}
+          sizes="100px"
+        ></Image>
+      </a>
     </footer>
   );
 }
