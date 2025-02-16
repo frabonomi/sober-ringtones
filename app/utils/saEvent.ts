27dab4802b4075747d49f10b2261f14fbeffa207
Francesco Bonomi <frbonomi@gmail.com>
2025-02-16 01:16:19 +0100

    Add events

---
+++ app/utils/saEvent.ts
@@ -0,0 +1,10 @@
+type SAMetadata = Record<string, string>;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+declare function sa_event(eventName: string, metadata?: SAMetadata): void;
+declare const window: Window & { sa_event: typeof sa_event };
+
+export const saEvent = (eventName: string, metadata?: SAMetadata) => {
+  console.log("saEvent", eventName, metadata);
+  if (window && window.sa_event) return window.sa_event(eventName, metadata);
+};
